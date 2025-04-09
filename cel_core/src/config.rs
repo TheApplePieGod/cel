e03744b91cda7dd46a96e85ccfa1df4dbc1053fc
TheApplePieGod <evant.002@gmail.com>
2025-04-09 16:48:35 -0400

    Dev config

--- cel_core/src/config.rs
+++ cel_core/src/config.rs
@@ -3,7 +3,11 @@ use std::path::PathBuf;
 pub fn get_config_dir() -> PathBuf {
     // TODO: this should probably be done once at startup
     let mut dir = dirs::data_local_dir().expect("Unable to locate config dir");
-    dir.push("cel");
+    if cfg!(debug_assertions) {
+        dir.push("cel_dev");
+    } else {
+        dir.push("cel");
+    };
     let _ = std::fs::create_dir_all(&dir);
     dir
 }
